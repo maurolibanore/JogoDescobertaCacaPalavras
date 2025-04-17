fdd63319b4fa84f0228210bd36c3a31507834b0e
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-04-16 22:28:10 -0300

    inclui o while no main para ele conseguir jogar novamente

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Vetor com palavras que podem ser usadas nos jogos
-        String[] palavras = {"agua", "peixe", "mauro", "frank, java"};
+        String[] palavras = {"agua", "peixe", "mauro", "frank", "java"};
 
 
-        // Exibe o menu de escolha do jogo
-        System.out.println("== Escolha o jogo: ==");
-        System.out.println("== 1 - Jogo da Descoberta ==");
-        System.out.println("== 2 - Caça-Palavras ==");
-        System.out.print("== Opção: ");
+        boolean jogarNovamente = true;
+        while (jogarNovamente) {
+            // Exibe o menu de escolha do jogo
+            System.out.println("== Escolha o jogo: ==");
+            System.out.println("== 1 - Jogo da Descoberta ==");
+            System.out.println("== 2 - Caça-Palavras ==");
+            System.out.print("== Opção: ");
 
-        int opcao = scanner.nextInt(); // Lê a opção escolhida
-        scanner.nextLine(); // Limpa o Enter deixado pelo nextInt
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        // Chama o jogo correspondente
-        if (opcao == 1) {
-            Descoberta descoberta = new Descoberta(palavras);
-            descoberta.iniciar();
-        } else {
-            JogoCacaPalavras cacaPalavras = new JogoCacaPalavras(palavras, 10);
-            cacaPalavras.iniciar();
+
+            if (opcao == 1) {
+                Descoberta descoberta = new Descoberta(palavras);
+                descoberta.iniciar();
+            } else if (opcao == 2) {
+                JogoCacaPalavras cacaPalavras = new JogoCacaPalavras(palavras, 10);
+                cacaPalavras.iniciar();
+            } else {
+                System.out.println("Opção inválida! Tente novamente.");
+                continue;
+            }
+
+            System.out.print("Você deseja jogar novamente? (s/n): ");
+            String resposta = scanner.nextLine();
+            if (resposta == "n") {
+                jogarNovamente = false;
+            }
         }
+
+        System.out.println("Obrigado por jogar!");
     }
 }
